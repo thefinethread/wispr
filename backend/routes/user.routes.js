297c24825ee7a3139eb5d97001ffee8ab7d1d6a4
Kartikey Shukla <kartikey8604@gmail.com>
2024-01-07 11:20:34 +0530

    setup authMiddleware and logout functionality

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { register, login } = require('../controllers/user.controller');
+const { register, login, logout } = require('../controllers/user.controller');
+const { verifyJwt } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.post('/logout', verifyJwt, logout);
 
 module.exports = router;
